28d6cdbfb0938b5421444d07271fc3dda0b52fea
吴青松 <1101982@qq.com>
2020-07-14 17:35:11 +0800

    modify

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+try {
+//    $image = \wqs\Image::open('./background.jpg');
+//    $image->crop(300, 300)->save("./images.png");
+} catch (Exception $e){
+    print_r($e);
+}
+
+
+
+
