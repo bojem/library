0d177bb3013e973582810ffe934ab94ed6bbc4bb
吴青松 <1101982@qq.com>
2021-04-22 16:36:11 +0800

    modify

--- src/helper/ArrayHelper.php
+++ src/helper/ArrayHelper.php
@@ -22,6 +22,99 @@ class ArrayHelper
         return $arr;
     }
 
+    /**
+     * 递归数组
+     *
+     * @param array $items
+     * @param string $idField
+     * @param int $pid
+     * @param string $pidField
+     * @return array
+     */
+    public static function itemsMerge(array $items, $pid = 0, $idField = "id", $pidField = 'pid', $child = '-')
+    {
+        $map = [];
+        $tree = [];
+        foreach ($items as &$it) {
+            $it[$child] = [];
+            $map[$it[$idField]] = &$it;
+        }
+
+        foreach ($items as &$it) {
+            $parent = &$map[$it[$pidField]];
+            if ($parent) {
+                $parent[$child][] = &$it;
+            } else {
+                $pid == $it[$pidField] && $tree[] = &$it;
+            }
+        }
+
+        unset($items, $map);
+
+        return $tree;
+    }
+
+    /**
+     * 传递一个子分类ID返回所有的父级分类
+     *
+     * @param array $items
+     * @param $id
+     * @return array
+     */
+    public static function getParents(array $items, $id)
+    {
+        $arr = [];
+        foreach ($items as $v) {
+            if ($v['id'] == $id) {
+                $arr[] = $v;
+                $arr = array_merge(self::getParents($items, $v['pid']), $arr);
+            }
+        }
+
+        return $arr;
+    }
+
+    /**
+     * 传递一个父级分类ID返回所有子分类
+     *
+     * @param $cate
+     * @param int $pid
+     * @return array
+     */
+    public static function getChilds($cate, $pid)
+    {
+        $arr = [];
+        foreach ($cate as $v) {
+            if ($v['pid'] == $pid) {
+                $arr[] = $v;
+                $arr = array_merge($arr, self::getChilds($cate, $v['id']));
+            }
+        }
+
+        return $arr;
+    }
+
+    /**
+     * 传递一个父级分类ID返回所有子分类ID
+     *
+     * @param $cate
+     * @param $pid
+     * @param string $idField
+     * @param string $pidField
+     * @return array
+     */
+    public static function getChildIds($cate, $pid, $idField = "id", $pidField = 'pid')
+    {
+        $arr = [];
+        foreach ($cate as $v) {
+            if ($v[$pidField] == $pid) {
+                $arr[] = $v[$idField];
+                $arr = array_merge($arr, self::getChildIds($cate, $v[$idField], $idField, $pidField));
+            }
+        }
+
+        return $arr;
+    }
 
 
 }
--- src/helper/StringHelper.php
+++ src/helper/StringHelper.php
@@ -23,7 +23,7 @@ class StringHelper
     }
 
     /**
-     * 生成随机数
+     * 获取随机字符串
      * @param int $len 字符串长度
      * @param bool $hasSpecial 是否有特殊字符
      * @return string
@@ -42,6 +42,35 @@ class StringHelper
     }
 
     /**
+     * 获取随机字符串
+     *
+     * @param $length
+     * @param bool $numeric
+     * @return string
+     */
+    public static function randStr1($length, $numeric = false)
+    {
+        $seed = base_convert(md5(microtime() . $_SERVER['DOCUMENT_ROOT']), 16, $numeric ? 10 : 35);
+        $seed = $numeric ? (str_replace('0', '', $seed) . '012340567890') : ($seed . 'zZ' . strtoupper($seed));
+
+        $hash = '';
+        if (!$numeric) {
+            $hash = chr(rand(1, 26) + rand(0, 1) * 32 + 64);
+            $length--;
+        }
+
+        $max = strlen($seed) - 1;
+        $seed = str_split($seed);
+        for ($i = 0; $i < $length; $i++) {
+            $hash .= $seed[mt_rand(0, $max)];
+        }
+
+        return $hash;
+    }
+
+
+
+    /**
      * 生成随机数
      * @param  integer  $length  长度
      * @param  integer $numeric 是否包含字母
@@ -57,6 +86,34 @@ class StringHelper
             $hash .= $seed[mt_rand(0, $max)];
         }
         return $hash;
+    }
+
+    /**
+     * 获取数字随机字符串
+     *
+     * @param bool $prefix 判断是否需求前缀
+     * @param int $length 长度
+     * @return string
+     */
+    public static function random1($prefix = false, $length = 8)
+    {
+        $str = $prefix ?? '';
+
+        return $str . substr(implode(null, array_map('ord', str_split(substr(uniqid(), 7, 13), 1))), 0, $length);
+    }
+
+    /**
+     * 生成随机数
+     *
+     * @param $merchant_id
+     * @return false|string
+     */
+    public static function random2($merchant_id)
+    {
+        $time_str = date('YmdHis');
+        $rand_code = rand(0, 999999);
+
+        return substr(md5($time_str . $rand_code . $merchant_id), 16, 32);
     }
 
     /**
@@ -328,5 +385,264 @@ class StringHelper
         }else{
             return false;
         }
+    }
+
+    /**
+     * 省略文字
+     *
+     * @param $text
+     * @param int $num
+     * @return string|string[]
+     */
+    public static function textOmit($string, $num = 26)
+    {
+        $letter = [];
+        for ($i = 0; $i < mb_strlen($string, 'UTF-8'); $i++) {
+            $letter[] = mb_substr($string, $i, 1, 'UTF-8');
+        }
+
+        $content = "";
+        foreach ($letter as $key => $l) {
+            if ($key + 1 == $num) {
+                $content .= '...';
+                break;
+            }
+
+            $content .= $l;
+        }
+
+        return $content;
+    }
+
+    /**
+     * 将一个字符串部分字符用*替代隐藏
+     *
+     * @param string $string 待转换的字符串
+     * @param int $bengin 起始位置，从0开始计数，当$type=4时，表示左侧保留长度
+     * @param int $len 需要转换成*的字符个数，当$type=4时，表示右侧保留长度
+     * @param int $type 转换类型：0，从左向右隐藏；1，从右向左隐藏；2，从指定字符位置分割前由右向左隐藏；3，从指定字符位置分割后由左向右隐藏；4，保留首末指定字符串
+     * @param string $glue 分割符
+     * @return bool|string
+     */
+    public static function hideStr($string, $bengin = 0, $len = 4, $type = 0, $glue = "@")
+    {
+        if (empty($string)) {
+            return false;
+        }
+
+        $array = [];
+        if ($type == 0 || $type == 1 || $type == 4) {
+            $strlen = $length = mb_strlen($string);
+
+            while ($strlen) {
+                $array[] = mb_substr($string, 0, 1, "utf8");
+                $string = mb_substr($string, 1, $strlen, "utf8");
+                $strlen = mb_strlen($string);
+            }
+        }
+
+        switch ($type) {
+            case 0 :
+                for ($i = $bengin; $i < ($bengin + $len); $i++) {
+                    isset($array[$i]) && $array[$i] = "*";
+                }
+
+                $string = implode("", $array);
+                break;
+            case 1 :
+                $array = array_reverse($array);
+                for ($i = $bengin; $i < ($bengin + $len); $i++) {
+                    isset($array[$i]) && $array[$i] = "*";
+                }
+
+                $string = implode("", array_reverse($array));
+                break;
+            case 2 :
+                $array = explode($glue, $string);
+                $array[0] = self::hideStr($array[0], $bengin, $len, 1);
+                $string = implode($glue, $array);
+                break;
+            case 3 :
+                $array = explode($glue, $string);
+                $array[1] = self::hideStr($array[1], $bengin, $len, 0);
+                $string = implode($glue, $array);
+                break;
+            case 4 :
+                $left = $bengin;
+                $right = $len;
+                $tem = array();
+                for ($i = 0; $i < ($length - $right); $i++) {
+                    if (isset($array[$i])) {
+                        $tem[] = $i >= $left ? "*" : $array[$i];
+                    }
+                }
+
+                $array = array_chunk(array_reverse($array), $right);
+                $array = array_reverse($array[0]);
+                for ($i = 0; $i < $right; $i++) {
+                    $tem[] = $array[$i];
+                }
+                $string = implode("", $tem);
+                break;
+        }
+
+        return $string;
+    }
+
+    /**
+     * php截取指定两个字符之间字符串，默认字符集为utf-8 Power by 大耳朵图图
+     * @param string $begin  开始字符串
+     * @param string $end    结束字符串
+     * @param string $str    需要截取的字符串
+     * @return string
+     */
+    public static function cut($begin,$end,$str){
+        $b = mb_strpos($str,$begin) + mb_strlen($begin);
+        $e = mb_strpos($str,$end) - $b;
+
+        return mb_substr($str,$b,$e);
+    }
+
+    /**
+     *  根据身份证号码获取性别
+     *  author:xiaochuan
+     *  @param string $idcard    身份证号码
+     *  @return int $sex 性别 1男 2女 0未知
+     */
+    public static function get_sex($idcard) {
+        if(empty($idcard)) return null;
+        $sexint = (int) substr($idcard, 16, 1);
+        return $sexint;
+    }
+
+    /**
+     *  根据身份证号码获取生日
+     *  author:xiaochuan
+     *  @param string $idcard    身份证号码
+     *  @return $birthday
+     */
+    public static function get_birthday($idcard) {
+        if(empty($idcard)) return null;
+        $bir = substr($idcard, 6, 8);
+        $year = (int) substr($bir, 0, 4);
+        $month = (int) substr($bir, 4, 2);
+        $day = (int) substr($bir, 6, 2);
+        return $year . "-" . $month . "-" . $day;
+    }
+
+    /**
+     *  根据身份证号码计算年龄
+     *  author:xiaochuan
+     *  @param string $idcard    身份证号码
+     *  @return int $age
+     */
+    public static function get_age($idcard){
+        if(empty($idcard)) return null;
+        #  获得出生年月日的时间戳
+        $date = strtotime(substr($idcard,6,8));
+        #  获得今日的时间戳
+        $today = strtotime('today');
+        #  得到两个日期相差的大体年数
+        $diff = floor(($today-$date)/86400/365);
+        #  strtotime加上这个年数后得到那日的时间戳后与今日的时间戳相比
+        $age = strtotime(substr($idcard,6,8).' +'.$diff.'years')>$today?($diff+1):$diff;
+        return $age;
+    }
+
+    /**
+     *  判断字符串是否是身份证号
+     *  author:xiaochuan
+     *  @param string $idcard    身份证号码
+     */
+    public static function isIdCard($idcard){
+        #  转化为大写，如出现x
+        $idcard = strtoupper($idcard);
+        #  加权因子
+        $wi = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
+        $ai = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
+        #  按顺序循环处理前17位
+        $sigma = 0;
+        #  提取前17位的其中一位，并将变量类型转为实数
+        for ($i = 0; $i < 17; $i++) {
+            $b = (int)$idcard{$i};
+            #  提取相应的加权因子
+            $w = $wi[$i];
+            #  把从身份证号码中提取的一位数字和加权因子相乘，并累加
+            $sigma += $b * $w;
+        }
+        #  计算序号
+        $sidcard = $sigma % 11;
+        #  按照序号从校验码串中提取相应的字符。
+        $check_idcard = $ai[$sidcard];
+        if ($idcard{17} == $check_idcard) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     *  根据身份证号，返回对应的生肖
+     *  author:xiaochuan
+     *  @param string $idcard    身份证号码
+     */
+    public static function get_zodiac($idcard){ //
+        if(empty($idcard)) return null;
+        $start = 1901;
+        $end = (int)substr($idcard, 6, 4);
+        $x = ($start - $end) % 12;
+        $val = '';
+        if ($x == 1 || $x == -11) $val = '鼠';
+        if ($x == 0)              $val = '牛';
+        if ($x == 11 || $x == -1) $val = '虎';
+        if ($x == 10 || $x == -2) $val = '兔';
+        if ($x == 9 || $x == -3)  $val = '龙';
+        if ($x == 8 || $x == -4)  $val = '蛇';
+        if ($x == 7 || $x == -5)  $val = '马';
+        if ($x == 6 || $x == -6)  $val = '羊';
+        if ($x == 5 || $x == -7)  $val = '猴';
+        if ($x == 4 || $x == -8)  $val = '鸡';
+        if ($x == 3 || $x == -9)  $val = '狗';
+        if ($x == 2 || $x == -10) $val = '猪';
+        return $val;
+    }
+
+    /**
+     *  根据身份证号，返回对应的星座
+     *  author:xiaochuan
+     *  @param string $idcard    身份证号码
+     */
+    public static function get_starsign($idcard){
+        if(empty($idcard)) return null;
+        $b = substr($idcard, 10, 4);
+        $m = (int)substr($b, 0, 2);
+        $d = (int)substr($b, 2);
+        $val = '';
+        if(($m == 1 && $d <= 21) || ($m == 2 && $d <= 19)){
+            $val = "水瓶座";
+        }else if (($m == 2 && $d > 20) || ($m == 3 && $d <= 20)){
+            $val = "双鱼座";
+        }else if (($m == 3 && $d > 20) || ($m == 4 && $d <= 20)){
+            $val = "白羊座";
+        }else if (($m == 4 && $d > 20) || ($m == 5 && $d <= 21)){
+            $val = "金牛座";
+        }else if (($m == 5 && $d > 21) || ($m == 6 && $d <= 21)){
+            $val = "双子座";
+        }else if (($m == 6 && $d > 21) || ($m == 7 && $d <= 22)){
+            $val = "巨蟹座";
+        }else if (($m == 7 && $d > 22) || ($m == 8 && $d <= 23)){
+            $val = "狮子座";
+        }else if (($m == 8 && $d > 23) || ($m == 9 && $d <= 23)){
+            $val = "处女座";
+        }else if (($m == 9 && $d > 23) || ($m == 10 && $d <= 23)){
+            $val = "天秤座";
+        }else if (($m == 10 && $d > 23) || ($m == 11 && $d <= 22)){
+            $val = "天蝎座";
+        }else if (($m == 11 && $d > 22) || ($m == 12 && $d <= 21)){
+            $val = "射手座";
+        }else if (($m == 12 && $d > 21) || ($m == 1 && $d <= 20)){
+            $val = "魔羯座";
+        }
+        return $val;
     }
 }
